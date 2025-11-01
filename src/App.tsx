fc918388fa97fbe0b891acf005b91ae34b2f4eff
ejqs <62688314+ejqs@users.noreply.github.com>
2025-11-01 16:43:10 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (<div>
+    <pre>
+      negros oriental based game studio<br />
+     _____<br />
+ ___ |[]|_n__n_I_c<br />
+|___||__|###|____<br />
+ O-O--O-O+++--O-<a href='https://www.asciiart.eu/vehicles/trains'>O</a><br />
+ contact us at ** hello |at| norail |d0t| co **
+    </pre>
+    </div>
+  )
+}
+
+export default App;
